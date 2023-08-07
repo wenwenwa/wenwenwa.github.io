6f9c603e416cc81a5dd1494622c374a8dc96fbde
Tang <11726782+sugar_gitee@user.noreply.gitee.com>
2023-08-07 09:41:37 +0800

    first commit

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"heroText\":\"Hello World!\",\"tagline\":\"积跬步以至千里，致敬每个爱学习的你!\",\"pageClass\":\"vdoing-index-class\",\"postList\":\"detailed\",\"hideRightBar\":false},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
